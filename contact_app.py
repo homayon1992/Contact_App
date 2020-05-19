f8bac2ddab91d8d037ae3b446d291ef6fad67a47
homayon <homayon623@gmail.com>
2020-05-19 12:09:09 -0700

    Edit Contact Class Complete the panel class

--- contact_app.py
+++ contact_app.py
@@ -9,18 +9,103 @@ class Contact:
         self.email = email
         self.address = address
 
+    def __str__(self):
+        return "name: {}\nlastname: {}\nphone: {}\nemail: {}\naddress: {}" \
+            .format(self.name, self.lastname, self.phone, self.email, self.address)
 
 
 class Panel:
 
+    def __init__(self):
+        self.repo = ListRepository()
+
+    def add_new_contact(self):
+        name = input("Enter Name... ")
+        lastname = input("Enter LastName... ")
+        phone = input("Enter Phone... ")
+        email = input("Enter E-mail... ")
+        address = input("Enter Address... ")
+        contact = Contact(name, lastname, phone, email, address)
+        self.repo.save(contact)
+
+    def show_all_contact(self):
+        contacts = self.repo.find_all()
+        for contact in contacts:
+            print(contact)
+            print("="*30)
+
+    def search_contact(self):
+        name = input("Enter Name... ")
+        contacts = self.repo.serch_by_name(name)
+        for contact in contacts:
+            print(contact)
+            print("="*30)
+
+
+    def delete_contact(self):
+        name = input("Enter Name... ")
+        contacts = self.repo.serch_by_name(name)
+        for contact in contacts:
+            print(contact)
+            print("=" * 30)
+            choice = input("Do You Want To Delete Contact? [Y/N]... ")
+            if choice == 'Y' or choice == 'y' :
+                self.repo.delete(contact._id)
+                break
+
+    def update_contact(self):
+        name = input("Enter Name... ")
+        contacts = self.repo.serch_by_name(name)
+        for contact in contacts:
+            print(contact)
+            print("=" * 30)
+            choice = input("Do You Want To Update Contact? [Y/N]... ")
+            if choice == 'Y' or choice == 'y':
+                name = input("Enter Name... ")
+                lastname = input("Enter LastName... ")
+                phone = input("Enter Phone... ")
+                email = input("Enter E-mail... ")
+                address = input("Enter Address... ")
+                name = contact.name if name == '' else name
+                lastname = contact.lastname if lastname == '' else lastname
+                phone = contact.phone if phone == '' else phone
+                email = contact.email if email == '' else email
+                address = contact.address if address == '' else address
+                new_contact = Contact(name, lastname, phone, email, address)
+                new_contact._id = contact._id
+                self.repo.update(new_contact)
+
     def show_menu(self):
-        pass
+        print("1-Add New Contact")
+        print("2-Show All Contact")
+        print("3-Search Contact By Name")
+        print("4-Delete Contact")
+        print("5-Update Contact")
+        print("6-Exit")
+
 
     def start_app(self):
-        pass
+        while True:
+            self.show_menu()
+            option = int(input("What Should I Do? [Number]... "))
+            if option == 1:
+                self.add_new_contact()
+            elif option == 2:
+                self.show_all_contact()
+            elif option == 3:
+                self.search_contact()
+            elif option == 4:
+                self.delete_contact()
+            elif option == 5:
+                self.update_contact()
+            elif option == 6:
+                exit(0)
+            else:
+                print("Please Enter a Number")
 
 
-class Repository:
+
+class ListRepository:
     def __init__(self):
         self.repositori = {}
 
@@ -42,3 +127,9 @@ class Repository:
 
     def update(self, contact):
         self.save(contact)
+
+
+
+
+App = Panel()
+App.start_app()
